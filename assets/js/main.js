42983b1d6f3d2f3a78f73d39104cb5ec2962e425
arthurcbastos <54586598+arthurcbastos@users.noreply.github.com>
2021-03-19 11:37:43 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+let menuList = document.getElementById("menuList");
+
+        menuList.style.maxHeight = "0px"
+
+        function togglemenu() {
+            if(menuList.style.maxHeight == "0px")
+            {
+                menuList.style.maxHeight = "160px";
+            }
+            else
+            {
+                menuList.style.maxHeight = "0px";
+            }
+        }
